cbbda079b9371457a17aa037d34be23ef743a8cc
DocDBrown <152238194+DocDBrown@users.noreply.github.com>
2024-05-29 15:07:04 +1000

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -13,36 +13,31 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.db' 
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY =\
-    'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'
+SECRET_KEY = 'django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0'  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost',
-'https://drdbrown-8000.theianext-0-labs-prod-misc-tools-us-east-0.
-proxy.cognitiveclass.ai',
-'https://drdbrown-8000.theianext-1-labs-prod-misc-tools-us-east-0.
-proxy.cognitiveclass.ai',
-'https://drdbrown-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.
-proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS=[
-'https://drdbrown-8000.theianext-0-labs-prod-misc-tools-us-east-0.
-proxy.cognitiveclass.ai',
-'https://drdbrown-8000.theianext-1-labs-prod-misc-tools-us-east-0.
-proxy.cognitiveclass.ai',
-'https://drdbrown-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01
-.proxy.cognitiveclass.ai']
+ALLOWED_HOSTS = [
+    'localhost',
+    'https://drdbrown-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # noqa
+    'https://drdbrown-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # noqa
+    'https://drdbrown-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'  # noqa
+]
+CSRF_TRUSTED_ORIGINS = [
+    'https://drdbrown-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # noqa
+    'https://drdbrown-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',  # noqa
+    'https://drdbrown-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'  # noqa
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -65,6 +60,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -94,7 +90,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -107,24 +102,18 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation
-        .UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME':
-        'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
@@ -138,7 +127,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
