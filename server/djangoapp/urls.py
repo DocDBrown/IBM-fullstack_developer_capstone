3151ab0cd03c7fa6f247592a6eabaa66fad6b29b
DocDBrown <152238194+DocDBrown@users.noreply.github.com>
2024-05-29 14:53:36 +1000

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -9,7 +9,7 @@ urlpatterns = [
     path('register', views.registration, name='register'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name='getcars'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships,
          name='get_dealers_by_state'),
