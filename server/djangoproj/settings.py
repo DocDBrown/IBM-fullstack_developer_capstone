8914b59ee0b0b340501824029dfe34cb9fc8873f
DocDBrown <152238194+DocDBrown@users.noreply.github.com>
2024-05-29 15:01:18 +1000

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,16 +29,16 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost', 
+ALLOWED_HOSTS=['localhost',
 'https://drdbrown-8000.theianext-0-labs-prod-misc-tools-us-east-0.
-proxy.cognitiveclass.ai', 
+proxy.cognitiveclass.ai',
 'https://drdbrown-8000.theianext-1-labs-prod-misc-tools-us-east-0.
 proxy.cognitiveclass.ai',
 'https://drdbrown-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.
 proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS=[
 'https://drdbrown-8000.theianext-0-labs-prod-misc-tools-us-east-0.
-proxy.cognitiveclass.ai', 
+proxy.cognitiveclass.ai',
 'https://drdbrown-8000.theianext-1-labs-prod-misc-tools-us-east-0.
 proxy.cognitiveclass.ai',
 'https://drdbrown-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01
