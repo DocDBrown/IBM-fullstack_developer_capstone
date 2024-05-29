1998e7b3ae5dfcc2f2fc41115132344a389e7fd2
DocDBrown <152238194+DocDBrown@users.noreply.github.com>
2024-05-29 14:01:44 +1000

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,5 @@
 # Uncomment the following imports before adding the Model code
 from django.db import models
-from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 class CarMake(models.Model):
@@ -9,6 +8,7 @@ class CarMake(models.Model):
 
     def __str__(self):
         return self.name
+
 
 class CarModel(models.Model):
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  
